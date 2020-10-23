bc42bb06df0e857ef5db15fff4c3d29f537295e9
wamuko <wamuko.kazu@gmail.com>
2020-10-23 15:09:34 +0900

    修正

--- thermometer.py
+++ thermometer.py
@@ -23,27 +23,28 @@ try:
     # センサーの初期化待ち
     time.sleep(.1)
 
-    with picamera.PiCamera() as camera:
-        camera.resolution = (320, 240)
-        camera.capture('./tmp.jpg')
+    while True:
+        with picamera.PiCamera() as camera:
+            camera.resolution = (320, 240)
+            camera.capture('./tmp.jpg')
 
-    max_temp = max(max(sensor.pixels))
-    print(max_temp)
-    img0 = cv2.imread('./tmp.jpg')
-    img = img0[0:240, 41:280]
-    img = img[:, :, ::-1].copy()
+        max_temp = max(max(sensor.pixels))
+        print(max_temp)
+        img0 = cv2.imread('./tmp.jpg')
+        img = img0[0:240, 41:280]
+        img = img[:, :, ::-1].copy()
 
-    plt.subplot(1, 2, 1)
-    fig = plt.imshow(sensor.pixels, cmap="inferno", interpolation="bicubic", vmin=min(min(sensor.pixels)), vmax=max(max(sensor.pixels)))
-    plt.colorbar()
+        plt.subplot(1, 2, 1)
+        fig = plt.imshow(sensor.pixels, cmap="inferno", interpolation="bicubic", vmin=min(min(sensor.pixels)), vmax=max(max(sensor.pixels)))
+        plt.colorbar()
 
-    plt.subplot(1, 2, 2)
-    plt.text(0, 0, str(max_temp) + 'deg', size=20, color="red")
-    plt.savefig("img.png")
+        plt.subplot(1, 2, 2)
+        plt.text(0, 0, str(max_temp) + 'deg', size=20, color="red")
+        plt.savefig("img.png")
 
-    plt.draw()
+        plt.draw()
 
-    plt.pause(0.03)
+        plt.pause(0.03)
     plt.clf()
 
 except KeyboardInterrupt:
